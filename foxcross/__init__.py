4f9fded43e218a1ddd5fc9dc9ce2ba62abc90978
Steven Pate <steven@laac.io>
2019-05-31 09:33:07 -0500

    adding version import to init

--- foxcross/__init__.py
+++ foxcross/__init__.py
@@ -1,4 +1,5 @@
 from .base_serving import ModelServing
 from .run_serving import compose_serving_models, run_model_serving
+from .__version__ import __version__  # noqa
 
 __all__ = ["ModelServing", "compose_serving_models", "run_model_serving"]
