258ff16bd5640bd3b50d3d642bee50b41ad06323
Steven Pate <steven@laac.io>
2019-05-31 08:36:38 -0500

    adding run and compose to api

--- foxcross/__init__.py
+++ foxcross/__init__.py
@@ -1,3 +1,4 @@
 from .base_serving import ModelServing
+from .run_serving import compose_serving_models, run_model_serving
 
-__all__ = ["ModelServing"]
+__all__ = ["ModelServing", "compose_serving_models", "run_model_serving"]
