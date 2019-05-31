68cab1a2c0d6756c1b3c22ae10423c1666127224
Steven Pate <steven@laac.io>
2019-05-31 09:46:35 -0500

    refactor running module

--- foxcross/__init__.py
+++ foxcross/__init__.py
@@ -1,5 +1,5 @@
 from .__version__ import __version__  # noqa
 from .base_serving import ModelServing
-from .run_serving import compose_serving_models, run_model_serving
+from .runner import compose_serving_models, run_model_serving
 
 __all__ = ["ModelServing", "compose_serving_models", "run_model_serving"]
