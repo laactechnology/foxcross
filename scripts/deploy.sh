0ca4fe72b9df8bfe0eabf6582563a36060cdf5d3
steven <steven@laac.io>
2019-06-12 16:13:52 -0500

    adding missing dunder version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git checkout master
 
+dunder_version=$( cat foxcross/__version__.py | grep version | sed "s/__version__ = //" | sed "s/'//g" )
 git tag -a ${dunder_version} -m ${dunder_version}
 git push --tags
 
